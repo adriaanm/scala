61f72026134d498db6abcefb5174384a0cf92892
Paul Phillips <paulp@improving.org>
2013-10-20 16:46:22 -0700

    Version bumps.

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,8 +7,8 @@ object Versions {
   // Versions of artifacts upon which we depend
   object Deps {
     def starr   = v("2.11.0-M4")
-    def parsers = v("1.0-RC1")
-    def xml     = v("1.0-RC2")
+    def parsers = v("1.0-RC2")
+    def xml     = v("1.0-RC4")
     def partest = v("1.0-RC4")
     def ant     = v("1.9.2")
     def jline   = v("2.11")
