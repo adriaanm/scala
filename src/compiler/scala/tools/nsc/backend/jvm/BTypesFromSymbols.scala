f0916846a17a2f10ee005b7a930166db1e3808e7
Adriaan Moors <adriaan.moors@typesafe.com>
2014-10-24 14:32:41 +0200

    SIP-23 Parse `Literal` as `SimpleType` under -Xexperimental
    `LiteralType` is a `SingleType` with a literal for its path.
    With an example: `1` is to `p.type`, as `1` is to `p`.
    
    `lit` is the most precise type for an expression known to evaluate to `lit`.
    Note that we cannot constant fold the expression to `lit`, since it may have a side effect.
    
    `ConstantType(c)` is the type of an expression that can be constant folded to `c`.
    
    When an expressions should not be constant folded, the corresponding `LiteralType`
    is created: `ConstantType(c).deconst == LiteralType(c)`
    (this is where all `LiteralType`s originate, except for unpickling).
    
    `typedLiteral` always infers a `ConstantType`, which is subsequently
    `deconst`ed or `widen`ed where necessary (see `widenIfNecessary`).
    In short, only `final val`s may have `ConstantType`s, and
    `widen`ing is required to ensure that a path is stable and accessible (roughly speaking).
    
    When checking subtyping, this invariant is useful: `LiteralType(c).widen == c.tpe`.
    
    TODO:
      - test! (subtyping, type inference, type tests, patmat, dependent method types)
      - fully predicate implementation on -Xsip:23
      - is it safe to reduce LITERAL in pickler?
      - symbols as literals?
    
    Notes:
     - `()` is not a literal
    
    Equality test for singleton type <:< AnyVal
    
    test case:
    
    ```
    val y: 5 = 5
    def g(x: Int) = x match {
      case _: y.type => 0
    }
    ```
    
    Accept negated literal in infixTypeRest
    
    e.g., val x : -1 => -1 = ???
    
    TODO positions in parser

--- src/compiler/scala/tools/nsc/backend/jvm/BTypesFromSymbols.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/BTypesFromSymbols.scala
@@ -207,6 +207,7 @@ class BTypesFromSymbols[G <: Global](val global: G) extends BTypes {
           case ThisType(sym)                      => classBTypeFromSymbol(sym)
           case SingleType(_, sym)                 => primitiveOrClassToBType(sym)
           case ConstantType(_)                    => typeToBType(t.underlying)
+          case LiteralType(_)                     => typeToBType(t.underlying)
           case RefinedType(parents, _)            => parents.map(typeToBType(_).asClassBType).reduceLeft((a, b) => a.jvmWiseLUB(b).get)
         }
     }
