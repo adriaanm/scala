869cdb813dc0e3bb7c7ea5bfc4020e35b59dcce6
Adriaan Moors <adriaan.moors@typesafe.com>
2016-02-11 09:37:06 -0800

    use relativize, not relativeInfo in abstract TR

--- src/reflect/scala/reflect/internal/Types.scala
+++ src/reflect/scala/reflect/internal/Types.scala
@@ -2048,7 +2048,7 @@ trait Types
       val symInfo = sym.info
       if (thisInfoCache == null || (symInfo ne symInfoCache)) {
         symInfoCache = symInfo
-        thisInfoCache = relativeInfo match {
+        thisInfoCache = relativize(symInfo) match {
           // If a subtyping cycle is not detected here, we'll likely enter an infinite
           // loop before a sensible error can be issued.  SI-5093 is one example.
           case x: SubType if x.supertype eq this =>
