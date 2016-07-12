c826320cb45f9255dc5dda5d4e503a117642843f
Adriaan Moors <adriaan@lightbend.com>
2016-07-11 23:17:20 -0700

    don't set annotations for the valdef -- we just want to compute the type sig

--- src/compiler/scala/tools/nsc/typechecker/Namers.scala
+++ src/compiler/scala/tools/nsc/typechecker/Namers.scala
@@ -813,22 +813,21 @@ trait Namers extends MethodSynthesis {
 
     /* Explicit isSetter required for bean setters (beanSetterSym.isSetter is false) */
     def accessorTypeCompleter(tree: ValDef, isSetter: Boolean) = mkTypeCompleter(tree) { sym =>
-      val accessorAnnots =
-        if (tree.mods.annotations.isEmpty) Nil
-        else filterAccessorAnnotations(annotSig(tree.mods.annotations), isSetter)
-
       // println(s"triaging for ${sym.debugFlagString} $sym from $valAnnots to $annots")
 
       // typeSig calls valDefSig (because tree: ValDef)
       // sym is an accessor, while tree is the field (which may have the same symbol as the getter, or maybe it's the field)
-      val valSig = typeSig(tree, accessorAnnots)
       // TODO: can we make this work? typeSig is called on same tree (valdef) to complete info for field and all its accessors
       // reuse work done in valTypeCompleter if we already computed the type signature of the val
       // (assuming the field and accessor symbols are distinct -- i.e., we're not in a trait)
-      //  if ((sym ne tree.symbol) && tree.symbol.isInitialized) tree.symbol.info
+      // val valSig =  if ((sym ne tree.symbol) && tree.symbol.isInitialized) tree.symbol.info
       //  else typeSig(tree)
+      val valSig = typeSig(tree, Nil) // don't set annotations for the valdef -- we just want to compute the type sig
 
       val sig = accessorSigFromFieldTp(sym, isSetter, valSig)
+
+      if (tree.mods.annotations.nonEmpty)
+        sym setAnnotations filterAccessorAnnotations(annotSig(tree.mods.annotations), isSetter)
 
       sym setInfo pluginsTypeSigAccessor(sig, typer, tree, sym)
 
