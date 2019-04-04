67f0a5e082b722a190192f92c62f85328eefc49f
Jason Zaugg <jzaugg@gmail.com>
2019-04-04 13:14:47 +1000

    Rework the way we unify Object and Any in Java/Scala interop
    References to `j.l.Object` from Java sources or classfiles use
    `definitions.ObjectTpeJava`, which behaves like `ObjectTpe`
    but is slackened to `AnyTpe` during `=:=` and `<:<` when this
    can help yield positive results.
    
    This subsumes two prior mechanisms (usages of `obj2Any` and `MethodType.isJava`)

--- test/files/pos/t11469/test.scala
+++ test/files/pos/t11469/test.scala
@@ -1,8 +1,4 @@
 import jkson.Generic
 
 abstract class ScalaGen[T] extends Generic[T]
-
-abstract class ScalaMono extends Generic[Product] {
-  // Crazy workaround for https://github.com/scala/scala-dev/issues/623
-  // override def foo(): Product with Object = super.foo().asInstanceOf[Product with Object]
-}
+abstract class ScalaMono extends Generic[Product] 
